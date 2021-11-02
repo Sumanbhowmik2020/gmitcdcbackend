9f21974845fc9beb56542ce1f85f3c1f0c100186
Sumanbhowmik2020 <sumanbhowmik700@gmail.com>
2021-11-02 21:49:33 +0530

    academic update

--- controllers/academicAPI_promise.js
+++ controllers/academicAPI_promise.js
@@ -18,13 +18,19 @@ const AcademicinfoModel = require('../models/academicinfo_model');
 }
 */
 
-
-router.post('/update', (req, res) => {
+router.post('/register', (req, res) => {
 
   //Create Object of Employee Model Class
   // And Receive value from request body and Store value within the Object
-  const academicinfoobj = new AcademicinfoModel({
-    studentname: req.body.studentname,
+  AcademicinfoModel.find({ $or: [{ "studentemail": req.body.studentemail }, { "studentmobile": req.body.studentmobile }] })
+    .then(response => {
+      if (response.length > 0) {
+        return res.send({ message: "You already upload your Academic Info" })
+      }
+      else {
+
+        const academicinfoobj = new AcademicinfoModel({
+              studentname: req.body.studentname,
     studentemail: req.body.studentemail,
     studentdepartment: req.body.studentdepartment,
     studentsession: req.body.studentsession,
@@ -44,18 +50,20 @@ router.post('/update', (req, res) => {
     studentcolpercentage: req.body.studentcolpercentage,
 
 
-  });//CLOSE EmpModel
-  //INSERT/SAVE THE RECORD/DOCUMENT
-  academicinfoobj.save()
-    .then(inserteddocument => {
-      res.status(200).send('DOCUMENT INSERED IN MONGODB DATABASE' + '<br\>' + inserteddocument);
-    })//CLOSE THEN
-    .catch(err => {
-      res.status(500).send({ message: err.message || 'Error in Employee Save ' })
-    });//CLOSE CATCH
+        });//CLOSE EmpModel
+        //INSERT/SAVE THE RECORD/DOCUMENT
+        academicinfoobj.save()
+          .then(inserteddocument => {
+            res.status(200).send({ message: "Academic Info Upload Successfull" });
+          })//CLOSE THEN
+
+          .catch(err => {
+            res.status(500).send({ message: err.message || 'Error in Employee Save ' })
+          });//CLOSE CATCH
+      }
+    })
 }//CLOSE CALLBACK FUNCTION BODY Line 27
 );//CLOSE POST METHOD Line 26
-
 
 router.get('/search/:emailid', (req, res) => {
   AcademicinfoModel.find({ "studentemail": req.params.emailid })
